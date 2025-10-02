c6ca2ce3f0e3bac5a76f5625b93c1916b2c54c74
Your Name <you@example.com>
2025-10-02 09:06:14 -0700

    continued changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
